b924dfe2e2f4e7fcdc6e738d3abcd8a4bb15ae48
Stanislaw Babkin <stalissss@outlook.com>
2021-12-05 19:56:38 +0300

    LRU Cache Implementation

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -28,3 +28,48 @@ func NewCache(capacity int) Cache {
 		items:    make(map[Key]*ListItem, capacity),
 	}
 }
+
+func (c *lruCache) Set(key Key, value interface{}) (exists bool) {
+	item, exists := c.items[key]
+	if exists {
+		item.Value = value
+		c.queue.MoveToFront(item)
+		return
+	}
+
+	item = c.queue.PushFront(value)
+	c.items[key] = item
+
+	if c.queue.Len() > c.capacity {
+		last := c.queue.Back()
+		var lastKey Key
+		for k, v := range c.items {
+			if v == last.Value {
+				lastKey = k
+				break
+			}
+		}
+		delete(c.items, lastKey)
+		c.queue.Remove(c.queue.Back())
+	}
+
+	return
+}
+
+func (c *lruCache) Get(key Key) (val interface{}, exists bool) {
+	item, exists := c.items[key]
+	if !exists {
+		val = nil
+		return
+	}
+
+	val = item.Value
+
+	c.queue.MoveToFront(item)
+	return
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue = NewList()
+}
