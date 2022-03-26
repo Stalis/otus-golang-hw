a36f218a5758c4d957d0e117c0fadc7396c783d1
Stanislaw Babkin <stalissss@outlook.com>
2022-03-26 19:12:39 +0300

    Base working version

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"errors"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrFileWriteNotAllowed   = errors.New("file write is not allowed")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	data, err := os.ReadFile(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	file, err := os.Create(toPath)
+	if err != nil {
+		return ErrFileWriteNotAllowed
+	}
+
+	data = data[offset:]
+	if limit > 0 && limit < int64(len(data)) {
+		data = data[:limit]
+	}
+
+	_, err = file.Write(data)
+	if err != nil {
+		return ErrFileWriteNotAllowed
+	}
+
 	return nil
 }
