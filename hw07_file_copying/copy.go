55c3a88499ac5485c96e35d67afc594c5f8e9dbd
Stanislaw Babkin <stalissss@outlook.com>
2022-03-27 20:34:48 +0300

    Add progress bar, move to bufferized copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,39 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"io"
 	"os"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrFileWriteNotAllowed   = errors.New("file write is not allowed")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrReadFailed            = errors.New("read failed")
 )
 
+const BufferSize = 100
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	data, err := os.ReadFile(fromPath)
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer fromFile.Close()
+
+	info, err := fromFile.Stat()
 	if err != nil {
 		return ErrUnsupportedFile
 	}
 
-	if offset > int64(len(data)) {
+	size := info.Size()
+	if offset > size {
 		return ErrOffsetExceedsFileSize
 	}
 
-	file, err := os.Create(toPath)
+	if limit > size {
+		limit = size
+	}
+
+	fromFile.Seek(offset, 0)
+
+	toFile, err := os.Create(toPath)
 	if err != nil {
 		return ErrFileWriteNotAllowed
 	}
+	defer toFile.Close()
 
-	data = data[offset:]
-	if limit > 0 && limit < int64(len(data)) {
-		data = data[:limit]
-	}
+	reader := bufio.NewReader(fromFile)
+	writer := bufio.NewWriter(toFile)
 
-	_, err = file.Write(data)
-	if err != nil {
-		return ErrFileWriteNotAllowed
+	pbar := progressbar.Default(size)
+	for {
+		n, err := io.CopyN(writer, reader, BufferSize)
+		pbar.Add64(n)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return ErrFileWriteNotAllowed
+		}
 	}
 
 	return nil
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,9 @@
 module github.com/Stalis/otus-golang-hw/hw07_file_copying
 
 go 1.16
+
+require (
+	github.com/schollz/progressbar/v3 v3.8.6 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+	golang.org/x/sys v0.0.0-20220325203850-36772127a21f // indirect
+)
