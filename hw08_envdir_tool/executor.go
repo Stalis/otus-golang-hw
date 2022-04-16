a13a97f383378db076e6ed3888605376afaa705c
Stanislaw Babkin <stalissss@outlook.com>
2022-04-16 16:43:47 +0300

    half-working variant

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func FindVar(envVars []string, name string) int {
+	for i, v := range envVars {
+		if curName, _, _ := strings.Cut(v, "="); curName == name {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func RemoveItem(s []string, i int) []string {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := exec.Command(cmd[0], cmd[1:]...)
+
+	keys := make([]string, 0, len(env))
+	for k := range env {
+		keys = append(keys, k)
+		if i := FindVar(command.Env, k); i > 0 {
+			command.Env = RemoveItem(command.Env, i)
+		}
+	}
+
+	for k, v := range env {
+		if v.NeedRemove {
+			continue
+		}
+
+		envVar := fmt.Sprintf("%v=%v", k, v.Value)
+		command.Env = append(command.Env, envVar)
+	}
+
+	command.Run()
+
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	return command.ProcessState.ExitCode()
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+
+	res := RunCmd(os.Args[2:], env)
+	os.Exit(res)
 }
